39a3d765f3a1b2ca8587e048a69000fcde874655
Mike Bett <mike.bett@student.moringa.school.com>
2025-06-12 15:18:55 +0300

    review table

--- models/reviews.py
+++ models/reviews.py
@@ -1,2 +1,11 @@
+from .user import db
+from datetime import datetime
 class reviews():
      __tablename__ = 'reviews'
+     id = db.column(db.integer, primary_key = True)
+     student_id = db.column(db.integer, db.ForeignKey('users.id'), nullable = False, unique = True)
+     course_id = db.column(db.integer, db.ForeignKey('courses.id'), nullable = False, unique = True)
+     rating = db.column(db.integer, nullable = True)
+     review = db.column(db.text)
+     created_at = db.column(db.Datetime, default = datetime.utcnow)
+     
