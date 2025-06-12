11828ceedf4f624202465bd3195e568352ef6479
Mike Bett <mike.bett@student.moringa.school.com>
2025-06-12 14:41:08 +0300

    courses table

--- models/courses.py
+++ models/courses.py
@@ -1,2 +1,12 @@
+from .user import db
+from datetime import datetime
+
 class courses():
      __tablename__ = 'courses'
+     
+     id = db.column(db.integer, primary_key = True)
+     title = db.column(db.string(255), nullable = False, unique = True)
+     description = db.column(db.text(255), nullable = False)
+     mentor_id = db.column(db.integer, db.ForeignKey('users.id'), nullable = False, unique = True)
+     created_at = db.column(db.Datetime, default = datetime.utcnow)
+     updated_at = db.column(db.datetime, default = datetime.utcnow, onupdate = datetime.utcnow)
