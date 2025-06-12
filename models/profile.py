54b91f68c02539c86f0b3b09773dc33d4c68c70d
Mike Bett <mike.bett@student.moringa.school.com>
2025-06-12 12:56:33 +0300

    profile table set

--- models/profile.py
+++ models/profile.py
@@ -1,2 +1,9 @@
+from .user import db
 class profile():
-     __tablename__ = 'profile'
+     __tablename__ = 'profiles'
+     id = db.column(db.integer, primary_key = True)
+     bio = db.column(db.text)
+     avatar_url = db.column(db.string(255))
+     user_id = db.column(db.integer, models.ForeignKey('users.id'),unique = True)
+     created_at = db.column(db.Datetime, default = datetime.utcnow)
+     updated_at = db.column(db.datetime, default = datetime.utcnow, onupdate = datetime.utcnow)
