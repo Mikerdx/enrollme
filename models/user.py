47a12f9012bbed59421dfff1b72d4438270afb9c
Mike Bett <mike.bett@student.moringa.school.com>
2025-06-12 13:06:09 +0300

    user table

--- models/user.py
+++ models/user.py
@@ -1,2 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import MetaData
+
+metadata = MetaData()
+db = SQLAlchemy(metadata=metadata)
 class user():
      __tablename__ = 'users'
+     
+     id = db.column(db.integer, primary_key = True)
+     username = db.column(db.string(255), nullable = False, unique = True)
+     email = db.column(db.string(255), nullable = False, unique = True)
+     password = db.column(db.varchar(8), nullable = False)
+     role = db.column(db.string(10), nullable = False)
+     created_at = db.column(db.Datetime, default = datetime.utcnow)
