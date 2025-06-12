a450ccf59a7eba7c9a9061c23f0923975f3452af
Mike Bett <mike.bett@student.moringa.school.com>
2025-06-12 14:30:25 +0300

    profile

--- models/profile.py
+++ models/profile.py
@@ -1,9 +1,10 @@
 from .user import db
+from datetime import datetime
 class profile():
      __tablename__ = 'profiles'
      id = db.column(db.integer, primary_key = True)
      bio = db.column(db.text)
      avatar_url = db.column(db.string(255))
-     user_id = db.column(db.integer, models.ForeignKey('users.id'),unique = True)
+     user_id = db.column(db.integer, db.ForeignKey('users.id'),unique = True)
      created_at = db.column(db.Datetime, default = datetime.utcnow)
      updated_at = db.column(db.datetime, default = datetime.utcnow, onupdate = datetime.utcnow)
