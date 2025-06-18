8bcd3feebffeee2e2aa53fbe3dcb5cdb4fac4ad1
Mike Bett <mike.bett@student.moringa.school.com>
2025-06-18 11:00:01 +0300

    done

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from .user import user
-from .courses import courses
-from .enrollment import enrollment
-from .profile import profile
-from .reviews import reviews
--- routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from .reviews_r import *
-from .enrollment_r import *
-from .user_r import *
-from .courses_r import *
-from .auth import auth_bp
-from .profile_r import profile_bp
